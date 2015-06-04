8b0ab303adb00d0b0832e7938c5a22360447f10c
Bjarne Varöystrand <black.skorpio+github@gmail.com>
2015-06-04 18:20:22 +0200

    Got it "semi-working"

--- plugin.php
+++ plugin.php
@@ -1,23 +1,34 @@
 <?php
 
-class kokenCookieConsent extends kokenPlugin {
-	function __construct() {
+class BaldursPhotographyCookieConsent extends KokenPlugin {
+
+	function __construct()
+	{
 		$this->require_setup = true;
 		$this->register_hook('before_closing_body', 'render');
 	}
-	function render() {
-		$path = $this->get_path();
+
+	function render()
+	{
+
+		$message	= $this->data->cookie_consent_message;
+		$dismiss	= $this->data->cookie_consent_dismiss;
+		$learnMore	= $this->data->cookie_consent_learnmore;
+		$link		= $this->data->cookie_consent_link;
+		$theme		= $this->data->cookie_consent_theme;
+		$path		= $this->get_path();
+
 		echo <<<OUT
-		<script>
-			window.cookieconsent_options = {
-				message: '{$this->data->cookie_consent_message}',
-				dismiss: '{$this->data->cookie_consent_dismiss}',
-				learnMore: '{$this->data->cookie_consent_learnmore}',
-				link: '{$this->data->cookie_consent_link}',
-				theme: '{$path}/styles/{$this->data->cookie_consent_theme}'
-			}
-		</script>
-		<script type="text/javascript" src="{$path}/cookieconsent.js"></script>
-		OUT;
+<script type="text/javascript">
+	window.cookieconsent_options = {
+		message: '{$message}',
+		dismiss: '{$dismiss}',
+		learnMore: '{$learnMore}',
+		link: '{$link}',
+		theme: '{$path}/styles/{$theme}.css'
+	}
+</script>
+<script type="text/javascript" src="{$path}/cookieconsent.js"></script>
+OUT;
 	}
 }
